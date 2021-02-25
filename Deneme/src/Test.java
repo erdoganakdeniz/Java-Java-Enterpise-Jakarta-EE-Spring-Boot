c1f29e8ef9c4ef491867e3426fc015a88d93ca14
erdoganakdeniz <erdo44@hotmail.de>
2021-02-26 01:35:39 +0300

    Yenilikler

---
+++ Deneme/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+public static void main(String[] args) {
+	Ogrenci ogrenci1=new Ogrenci();
+	ogrenci1.isim="";
+	ogrenci1.soyisim="asd";
+	ogrenci1.dogumYili=1234;
+	ogrenci1.okulNumarasi="";
+	
+	
+
+}
+}
